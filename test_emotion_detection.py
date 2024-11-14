4554a63db02916d10e36c3fbb668764008695e5f
VenWer82 <werven321@gmail.com>
2024-11-14 12:31:15 -0500

    finalized testing

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -9,5 +9,14 @@ class TestEmotionDetector(unittest.TestCase):
         res2 = emotion_detector('I am really mad about this')
         self.assertEqual(res2['dominant_emotion'], 'anger', 'Anger test failed')
 
+        res3 = emotion_detector('I feel disgusted just hearing about this')
+        self.assertEqual(res3['dominant_emotion'], 'disgust', 'Disgust test failed')
+
+        res4 = emotion_detector('I am so sad about this')
+        self.assertEqual(res4['dominant_emotion'], 'sadness', 'Sadness test failed')
+
+        res5 = emotion_detector('I am really afraid that this will happen')
+        self.assertEqual(res5['dominant_emotion'], 'fear', 'Fear test failed')
+        
 if __name__ == '__main__':
     unittest.main()
