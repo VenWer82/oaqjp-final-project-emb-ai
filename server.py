fcd347fdf58e6f9346af3f8ec6b445aa837acb30
VenWer82 <werven321@gmail.com>
2024-11-15 07:10:02 -0500

    finalize error handling

--- server.py
+++ server.py
@@ -13,6 +13,8 @@ def render_index_page():
 def emotion_detect():
     text_to_analyze = request.args.get('textToAnalyze') 
     result = emotion_detector(text_to_analyze)
+    if result['dominant_emotion'] == None:
+        return 'Invalid text! Please try again!'
     anger, disgust, fear, joy, sadness, dominant_emotion = result.values()
     return f'''For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}
              and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}.'''
