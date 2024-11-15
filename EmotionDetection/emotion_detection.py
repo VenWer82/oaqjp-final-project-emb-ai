96d503208292ba623e9417346dfd21b2ed347d4b
VenWer82 <werven321@gmail.com>
2024-11-15 05:27:27 -0500

    incorp error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,9 +5,14 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json =  {"raw_document": {"text": text_to_analyze}}
     response = requests.post(url, json=input_json, headers=headers)
-    data = json.loads(response.text) 
-    emotions_dict = data['emotionPredictions'][0]['emotion']
-    emotion, score = max(emotions_dict.items(), key= lambda x: x[1])
-    anger, disgust, fear, joy, sadness = emotions_dict.values()
-    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': emotion}
+    if response.status_code == 400:
+        anger = disgust = fear = joy = sadness = dom_emotion = None
+
+    elif response.status_code == 200:    
+        data = json.loads(response.text) 
+        emotions_dict = data['emotionPredictions'][0]['emotion']
+        dom_emotion, score = max(emotions_dict.items(), key= lambda x: x[1])
+        anger, disgust, fear, joy, sadness = emotions_dict.values()
+
+    return {'anger': anger, 'disgust': disgust, 'fear': fear, 'joy': joy, 'sadness': sadness, 'dominant_emotion': dom_emotion}
     
